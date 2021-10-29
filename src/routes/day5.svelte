c5c677a1c7094af4768ea7ca984b9aa9f62e0b9e
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-29 08:40:06 -0700

    cleanup

--- src/routes/day5.svelte
+++ src/routes/day5.svelte
@@ -1,86 +1,57 @@
 <script context="module">
     export const prerender = true;
     import p1 from '$lib/images/thumb/20211010_132048_Thumb.jpg';
-    // 20211010_132048-2.jpg
     import p2 from '$lib/images/thumb/20211010_133144_HDR_Thumb.jpg';
-    // 20211010_133144_HDR2.jpg
     import p3 from '$lib/images/thumb/20211010_133147_HDR_Thumb.jpg';
-    // 20211010_133147_HDR2.jpg
     import p4 from '$lib/images/thumb/20211010_133151_HDR_Thumb.jpg';
-    // 20211010_133151_HDR2.jpg
     import p5 from '$lib/images/thumb/20211010_133407_HDR_Thumb.jpg';
-    // 20211010_133407_HDR2.jpg
     import p6 from '$lib/images/thumb/20211010_133420_HDR_Thumb.jpg';
-    // 20211010_133420_HDR2.jpg
     import p7 from '$lib/images/thumb/20211010_133427_HDR_Thumb.jpg';
-    // 20211010_133427_HDR2.jpg
     import p8 from '$lib/images/thumb/20211010_133432_HDR_Thumb.jpg';
-    // 20211010_133432_HDR2.jpg
-
-
     import p9 from '$lib/images/thumb/20211010_134023_HDR_Thumb.jpg';
-    // 20211010_134023_HDR2.jpg
     import p10 from '$lib/images/thumb/20211010_134028_HDR_Thumb.jpg';
-    // 20211010_134028_HDR2.jpg
     import p11 from '$lib/images/thumb/20211010_134037_HDR_Thumb.jpg';
-    // 20211010_134037_HDR2.jpg
     import p12 from '$lib/images/thumb/20211010_134950_Thumb.jpg';
-    // 20211010_134950-2.jpg
     import p13 from '$lib/images/thumb/20211010_134954_HDR_Thumb.jpg';
-    // 20211010_134954_HDR2.jpg
     import p14 from '$lib/images/thumb/20211010_134959_HDR_Thumb.jpg';
-    // 20211010_134959_HDR2.jpg
     import p15 from '$lib/images/thumb/20211010_141105_HDR_Thumb.jpg';
-    // 20211010_141105_HDR2.jpg
     import p16 from '$lib/images/thumb/20211010_141107_HDR_Thumb.jpg';
-    // 20211010_141107_HDR2.jpg
-
-
-
     import p17 from '$lib/images/thumb/20211010_141549_HDR_Thumb.jpg';
-    // 20211010_141549_HDR2.jpg
     import p18 from '$lib/images/thumb/20211010_141553_HDR_Thumb.jpg';
-    // 20211010_141553_HDR2.jpg
     import p19 from '$lib/images/thumb/20211010_141555_HDR_Thumb.jpg';
-    // 20211010_141555_HDR2.jpg
     import p20 from '$lib/images/thumb/20211010_141600_HDR_Thumb.jpg';
-    // 20211010_141600_HDR2.jpg
     import p21 from '$lib/images/thumb/20211010_144813_Burst01_Thumb.jpg';
-    // 20211010_144813_Burst01-2.jpg
     import p22 from '$lib/images/thumb/20211010_164841_Thumb.jpg';
-    // 20211010_164841-2.jpg
     import p23 from '$lib/images/thumb/20211010_164844_Thumb.jpg';
-    // 20211010_164844-2.jpg
 </script>
 
 <div class="column">
-    <img src={ p1 } alt="fuckit">
-    <img src={ p2 } alt="fuckit">
-    <img src={ p3 } alt="fuckit">
-    <img src={ p4 } alt="fuckit">
-    <img src={ p5 } alt="fuckit">
-    <img src={ p9 } alt="fuckit">
-    <img src={ p10 } alt="fuckit">
-    <img src={ p11 } alt="fuckit">
-    <img src={ p12 } alt="fuckit">
-    <img src={ p13 } alt="fuckit">
-    <img src={ p14 } alt="fuckit">
-    <img src={ p15 } alt="fuckit">
-    <img src={ p16 } alt="fuckit">
-    <img src={ p17 } alt="fuckit">
-    <img src={ p18 } alt="fuckit">
-    <img src={ p19 } alt="fuckit">
-    <img src={ p20 } alt="fuckit">
-    <img src={ p21 } alt="fuckit">
+    <a href="/day5zoom/p1zoom"><img src={ p1 } alt="fuckit"></a>
+    <a href="/day5zoom/p2zoom"><img src={ p2 } alt="fuckit"></a>
+    <a href="/day5zoom/p3zoom"><img src={ p3 } alt="fuckit"></a>
+    <a href="/day5zoom/p4zoom"><img src={ p4 } alt="fuckit"></a>
+    <a href="/day5zoom/p5zoom"><img src={ p5 } alt="fuckit"></a>
+    <a href="/day5zoom/p9zoom"><img src={ p9 } alt="fuckit"></a>
+    <a href="/day5zoom/p10zoom"><img src={ p10 } alt="fuckit"></a>
+    <a href="/day5zoom/p11zoom"><img src={ p11 } alt="fuckit"></a>
+    <a href="/day5zoom/p12zoom"><img src={ p12 } alt="fuckit"></a>
+    <a href="/day5zoom/p13zoom"><img src={ p13 } alt="fuckit"></a>
+    <a href="/day5zoom/p14zoom"><img src={ p14 } alt="fuckit"></a>
+    <a href="/day5zoom/p15zoom"><img src={ p15 } alt="fuckit"></a>
+    <a href="/day5zoom/p16zoom"><img src={ p16 } alt="fuckit"></a>
+    <a href="/day5zoom/p17zoom"><img src={ p17 } alt="fuckit"></a>
+    <a href="/day5zoom/p18zoom"><img src={ p18 } alt="fuckit"></a>
+    <a href="/day5zoom/p19zoom"><img src={ p19 } alt="fuckit"></a>
+    <a href="/day5zoom/p20zoom"><img src={ p20 } alt="fuckit"></a>
+    <a href="/day5zoom/p21zoom"><img src={ p21 } alt="fuckit"></a>
 </div>
 <div>
-    <img src={ p6 } alt="fuckit">
-    <img src={ p7 } alt="fuckit">
-    <img src={ p8 } alt="fuckit">
-    <img src={ p22 } alt="fuckit">
-    <img src={ p23 } alt="fuckit">
+    <a href="/day5zoom/p6zoom"><img src={ p6 } alt="fuckit"></a>
+    <a href="/day5zoom/p7zoom"><img src={ p7 } alt="fuckit"></a>
+    <a href="/day5zoom/p8zoom"><img src={ p8 } alt="fuckit"></a>
+    <a href="/day5zoom/p22zoom"><img src={ p22 } alt="fuckit"></a>
+    <a href="/day5zoom/p23zoom"><img src={ p23 } alt="fuckit"></a>
 </div>
-
 
 <style>
 	.column {
